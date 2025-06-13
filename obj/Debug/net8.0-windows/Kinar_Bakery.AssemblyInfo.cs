ef7432f04d74e8df51008b3c75526f2afc6cb2f3
rivatdfryn <rivatdefryanto001@gmail.com>
2025-06-14 00:39:01 +0700

    tinggal rapihin

--- obj/Debug/net8.0-windows/Kinar_Bakery.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/Kinar_Bakery.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Kinar_Bakery")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e04e2cd2348f121866717322a07a946649cc01b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9032dedd4663d88ff7fff5a966e8e4fcc50144c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Kinar_Bakery")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Kinar_Bakery")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
